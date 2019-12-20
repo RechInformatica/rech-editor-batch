9e921e0c47819166e0b1b58b659fcbc0331b10a0
Vanduir <vanduir@rech.com.br>
2019-12-20 17:30:30 -0300

    Created the new commentary line above and the tab stop controll

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext } from 'vscode';
+import { Editor } from 'rech-editor-cobol';
+import { BatchTabStopper } from './bat/BatchTabStopper';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "rech-editor-batch" is now active!');
-
+export function activate(_context: any) {
+    const context = <ExtensionContext>_context;
+    //
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    //
+    context.subscriptions.push(commands.registerCommand('rech.editor.batch.batchInsertCommentLine', async () => {
+        await new Editor().insertLineAbove();
+        commands.executeCommand("editor.action.commentLine");
+    }));
+    context.subscriptions.push(commands.registerCommand('rech.editor.batch.tab', () => {
+        new BatchTabStopper().processTabKey(true);
+    }));
+    context.subscriptions.push(commands.registerCommand('rech.editor.batch.revtab', () => {
+        new BatchTabStopper().processTabKey(false);
+    }));
 }
 
 // this method is called when your extension is deactivated
